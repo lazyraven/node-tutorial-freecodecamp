5810cd313c328202326dc40031e7894645278c73
DESKTOP-6FU4FD9\lazyraven <lazyraven1703@gmail.com>
2023-09-23 12:34:10 +0530

    node tutorial first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Globals - NO Window
+
+// _dirname  - path to current directory
+// _filename -  file name
+// requires - function to use modules (Common Js)
+// module - info about current module (file)
+// process - info about env where the program is being executed
+
+console.log(_dirname);
